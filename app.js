23284a41c4e6c54545c0fec3265c0d4a7603e28e
Masayuki Ota <masota@microsoft.com>
2018-03-12 17:42:10 +0900

    add core logic for dialog flow. need to add ms graph booking room function

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("test");
+const express = require('express');
+const bodyParser = require('body-parser');
+const DialogflowApp = require('actions-on-google').DialogflowApp;
+
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/gghome',(req,res)=>{
+    console.log(JSON.stringify(req.body));
+    
+    const dialogApp = new DialogflowApp({
+        request : req,
+        response : res
+    });
+    dialogApp.tell("やりがいあるなぁ～");
+    dialogApp.tell("ゴソゴソ...確保完了です！");
+});
+
+app.listen(process.env.PORT || 8080);
